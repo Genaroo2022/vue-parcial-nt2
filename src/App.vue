eaadd0eef62c74abbd86b7b600c29c419725f560
genaro <genarorotstein271@gmail.com>
2025-05-26 20:13:05 -0300

    Primer commit del primer parcial de Vue.js

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+import Formulario from "./components/Formulario.vue";
+
+export default {
+  name: 'app',
+  components: {
+    Formulario,
+  }
+}
+</script>
+
+<template>
+  <div class="class container-fluid mt-3">
+    <div class="class card">
+      <header class="class card-header">
+        <h2>NT2 Parcial</h2>
+      </header>
+
+      <main class="class card-body">
+        
+        <Formulario />
+
+      </main>
+
+
+
+
+    </div>
+
+
+
+
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,74 @@
+<template>
+  <section class="card">
+    
+    <form>
+      <div class="form-group">
+        <input type="text" v-model="nombre" class="form-control" />
+        <p>Cantidad caracteres = {{ cantidadCaracteres }}</p>
+        <ol>
+          <li>{{ codificado }} (codificado)</li>
+          <li>{{ mayuscula }} (mayuscula)</li>
+          <li>{{ minuscula }} (minuscula)</li>
+          <li>{{ intercaladoMayuscula }} (mayuscula / minuscula)</li>
+          <li>{{ intercaladoMinuscula }} (minuscula / mayuscula)</li>
+        </ol>
+      </div>
+    </form>
+  </section>
+  
+  <br>
+<p>Respuestas: 1:C - 2:C - 3:A</p>
+</template>
+
+<script>
+export default {
+  name: 'formulario',
+  data() {
+    return {
+      nombre: ''
+    };
+  },
+  computed: {
+    cantidadCaracteres() {
+      return this.nombre.length;
+    },
+    codificado() {
+      const reemplazo = { a: 'u', e: 'o', i: 'i', o: 'e', u: 'a' };
+      return this.nombre
+        .toLowerCase()
+        .split('')
+        .map(c => reemplazo[c] || c)
+        .join('');
+    },
+    mayuscula() {
+      return this.nombre.toUpperCase();
+    },
+    minuscula() {
+      return this.nombre.toLowerCase();
+    },
+    intercaladoMayuscula() {
+      return this.nombre
+        .split('')
+        .map((c, i) =>
+          i % 2 === 0 ? c.toUpperCase() : c.toLowerCase()
+        )
+        .join('');
+    },
+    intercaladoMinuscula() {
+      return this.nombre
+        .split('')
+        .map((c, i) =>
+          i % 2 === 0 ? c.toLowerCase() : c.toUpperCase()
+        )
+        .join('');
+    }
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  padding: 20px;
+  font-family: sans-serif;
+}
+</style>
